1d37c5e26792199733487ec95934b48427e1df5e
Prof. Márcio Torres <marcio.torres@riogrande.ifrs.edu.br>
2024-03-19 22:37:57 -0300

    Create encadeamento.js

---
+++ encadeamento.js
@@ -0,0 +1,14 @@
+function mult(a, b) { // função polimórfica
+  return a * b
+}
+
+function soma(a, b) {
+  return a + b
+}
+
+console.log(mult(2, "tads")) // tadstads
+console.log(mult("q", "w"))
+console.log(soma("q", "w"))
+console.log(soma(mult(4, 5), mult(6, soma(2, 1))))
+//console.log(+(* (4, 5), * (6, +(2, 1))))
+console.log(38)
